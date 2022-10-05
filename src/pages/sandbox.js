7a08e3031b81c1bef26b98eafb642e86975cc1b0
Edgar Carrillo <carrie01@thryv.com>
2022-10-05 10:37:09 -0700

    Add sandbox page

---
+++ src/pages/sandbox.js
@@ -0,0 +1,17 @@
+
+import { SandboxContainer } from '../components'
+
+/**
+ * 
+ * Feel free to add any components as children to <SandboxContainer /> 
+ * that you'd like to test!
+ * 
+ */
+
+export const Sandbox = () => {
+  return (
+    <SandboxContainer>
+
+    </SandboxContainer>
+  )
+}
